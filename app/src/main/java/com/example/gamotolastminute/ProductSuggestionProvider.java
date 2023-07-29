5ccfb3f3927352a31a0fc57e04d46329ac9b2fbe
Hamza Macabato <macabato.hmz@gmail.com>
2023-07-29 15:24:58 +0800

    test commit

--- app/src/main/java/com/example/gamotolastminute/ProductSuggestionProvider.java
+++ app/src/main/java/com/example/gamotolastminute/ProductSuggestionProvider.java
@@ -6,6 +6,8 @@ public class ProductSuggestionProvider extends SearchRecentSuggestionsProvider {
     public static final String AUTHORITY = "com.example.gamotolastminute.provider.ProductSuggestionProvider";
     public static final int MODE = DATABASE_MODE_QUERIES;
 
+    //comment
+
     public ProductSuggestionProvider() {
         setupSuggestions(AUTHORITY, MODE);
     }
